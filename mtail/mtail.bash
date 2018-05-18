767e82fdea25b5fb49898a7d90f836cd7a9a999c
Ahmet Alp Balkan <ahmetb@google.com>
2018-05-18 16:02:39 -0700

    Add license to .yaml files
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

--- mtail/mtail.bash
+++ mtail/mtail.bash
@@ -4,16 +4,17 @@ set -eo pipefail
 test -z "${1}" && echo "label selector required." 1>&2 && exit 1
 
 selector="$1"
-
 mapfile -t arr < <(kubectl get pods -l="${selector}" \
+    -n="${KUBECTL_PLUGINS_CURRENT_NAMESPACE}" \
     -o=jsonpath='{range .items[*].metadata.name}{@}{"\n"}{end}')
 
 for po in "${arr[@]}"; do
     (
-        kubectl logs --follow "${po}" | \
-            sed "s/^/$(tput setaf 3)[${po}] $(tput sgr0)/"
-            # TODO(ahmetb) add more colors and pick one for each pod
-    )&
+        set -ex
+        kubectl logs --follow "${po}" --tail=10 \
+            -n="${KUBECTL_PLUGINS_CURRENT_NAMESPACE}"
+    ) | sed "s/^/$(tput setaf 3)[${po}] $(tput sgr0)/" &
+    # TODO(ahmetb) add more colors and pick one for each pod
 done
 
 trap "exit" INT TERM ERR
