53b5641a9433b6020d27672a0213c345c6c8c0c6
Ahmet Alp Balkan <ahmetb@google.com>
2018-05-18 16:16:18 -0700

    Updates to extract-context.bash
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

--- extract-context/extract-context.bash
+++ extract-context/extract-context.bash
@@ -4,17 +4,18 @@
 set -eo pipefail
 
 
-test -z "$1" && echo "context name required." 1>&2 && exit 1
+test -z "$1" && echo "error: context name required" 1>&2 && exit 1
 ctx="$1"
 
 cur_ctx="$(kubectl config current-context)"
 if [[ -z "${cur_ctx}" ]]; then
-    echo >&2 "current context is not set"
+    echo >&2 "error: current context is not set"
     exit 1
 fi
 
+# switch back
 kubectl config use-context "${ctx}" || (
-    echo >&2 "failed to switch to context"
+    echo >&2 "error: failed to switch to context"
     exit 1
 )
 
@@ -22,4 +23,5 @@ kubectl config view --minify --flatten | \
     grep -v 'access-token:' | \
     grep -v 'expiry:'
 
+# switch back
 kubectl config use-context "${cur_ctx}" 2>&1 1>/dev/null
