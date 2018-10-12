3802e1cae8c037e73d0fec7bc9519c1bca9d25a1
Ahmet Alp Balkan <ahmetb@google.com>
2018-10-12 10:18:22 -0700

    mtail: remove namespace support temporarily

--- mtail/mtail.sh
+++ mtail/mtail.sh
@@ -24,14 +24,12 @@ test -z "${1}" && echo "label selector required." 1>&2 && exit 1
 
 selector="$1"
 mapfile -t arr < <(kubectl get pods -l="${selector}" \
-    -n="${KUBECTL_PLUGINS_CURRENT_NAMESPACE}" \
     -o=jsonpath='{range .items[*].metadata.name}{@}{"\n"}{end}')
 
 for po in "${arr[@]}"; do
     (
         set -ex
         kubectl logs --follow "${po}" --tail=10 \
-            -n="${KUBECTL_PLUGINS_CURRENT_NAMESPACE}"
     ) | sed "s/^/$(tput setaf 3)[${po}] $(tput sgr0)/" &
     # TODO(ahmetb) add more colors and pick one for each pod
 done
